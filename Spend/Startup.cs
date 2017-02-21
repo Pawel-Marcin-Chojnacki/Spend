43814e6430525536e7c60ff7e5641304f3c269c4
Paweł Marcin Chojnacki <pawel.marcin.chojnacki@gmail.com>
2017-02-21 13:10:30 +0100

    Initialize project template.

---
+++ Spend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Spend.Startup))]
+namespace Spend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
